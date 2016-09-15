d6268caf8b6fe130e5003b8c4e74d2ad6337edbc
Josh Fabean <joshfabean@gmail.com>
2016-09-15 11:39:13 -0500

    added some docker functions

--- updateThings.sh
+++ updateThings.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 # update drupal console
+echo 'updating Drupal console';
 /usr/local/bin/drupal self-update;
 
 # update nvm
+echo 'updating nvm';
 /usr/bin/curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | bash;w;
 
 # update brew things
+echo 'updating brew things';
 /usr/local/bin/brew update && brew install `brew outdated`;
 
+# update pip & all pip things
+echo 'update pip & things';
+/usr/local/bin/pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs pip install -U
