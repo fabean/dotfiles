888698f57d3b88e8766a742c5e996d2c0124215c
Josh Fabean <joshfabean@gmail.com>
2019-05-29 17:51:17 -0500

    added fish shell

---
+++ .config/fish/config.fish
@@ -0,0 +1,40 @@
+alias vim='nvim'
+alias bim='nvim'
+alias ssh="ssh -A" # forward ssh keys to server
+alias cp='cp -iv' # prompt when overwriting and verbose
+alias mv='mv -iv' # prompt when overwriting and verbose
+alias ll='ls -FGlAhp'
+alias du1='du -h -d 1'
+alias ls='exa'
+alias gs='git status'
+alias gap='git add -p'
+
+alias docker-composer='docker-compose'
+alias docker-start='systemctl start docker'
+alias docker-stop='systemctl stop docker'
+
+function vpn-start
+  sudo systemctl start openvpn-client@$1
+end
+
+function vpn-stop
+  sudo systemctl stop openvpn-client@$1
+end
+
+function vpn-status
+  sudo systemctl status openvpn-client@$1
+end
+
+# my path
+export PATH="/usr/local/bin/php:/usr/local/bin:/usr/local/sbin:/opt/local/bin:/opt/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/git/bin:$PATH"
+export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
+
+npm set prfix ~/.npm
+export PATH="$HOME/.npm/bin:$PATH"
+export PATH="./node_modules/.bin:$PATH"
+
+export LIBVA_DRIVER_NAME=vdpau
+export EDITOR=nvim
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk
+export LC_ALL="en_US.UTF-8"
+
