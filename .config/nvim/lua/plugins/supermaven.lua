4ed95974e87641efb9e3f0000ab0a9ff4ce246ed
Josh Fabean <me@joshfabean.com>
2025-09-26 14:12:05 -0400

    stuff

--- .config/nvim/lua/plugins/supermaven.lua
+++ .config/nvim/lua/plugins/supermaven.lua
@@ -8,9 +8,9 @@ return {
                     clear_suggestion = "<C-h>",
                     accept_word = "<C-w>",
                 },
-                log_level = "info",                -- Set to "off" to disable logging
-                disable_inline_completion = false, -- Enable inline completion instead of nvim-cmp integration
-                disable_keymaps = false            -- Set to true for manual keymap control
+                log_level = "off",                -- Disable logging
+                disable_inline_completion = true, -- Disable inline completion to prevent interference
+                disable_keymaps = false            -- Keep keymaps for manual control
             })
             
             -- Custom highlighting for supermaven suggestions
