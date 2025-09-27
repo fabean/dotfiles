11c5f75f8ff86ba6e063a9b9c0ae92d8d90d69b9
Josh Fabean <me@joshfabean.com>
2025-09-27 14:55:25 -0400

    move to opencode remove other stuff

--- .config/nvim/lua/plugins/cursor-agent.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "xTacobaco/cursor-agent.nvim",
-  config = function()
-    vim.keymap.set("n", "<leader>ca", ":CursorAgent<CR>", { desc = "Cursor Agent: Toggle terminal" })
-    vim.keymap.set("v", "<leader>ca", ":CursorAgentSelection<CR>", { desc = "Cursor Agent: Send selection" })
-    vim.keymap.set("n", "<leader>cA", ":CursorAgentBuffer<CR>", { desc = "Cursor Agent: Send buffer" })
-  end,
-}
